c7ab6237053d2d819e7fc8c21084e96c0a2a2326
Lyalya92 <lialia.suk@gmail.com>
2022-12-28 17:26:06 +0400

    Homework 2 + entities update

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,24 +1,32 @@
 CREATE TABLE products (
     id          bigserial primary key,
     title       varchar(255),
-    price       numeric (8, 2)
+    price       numeric (8, 2),
+    created_at  timestamp default current_timestamp,
+    updated_at  timestamp default current_timestamp
 );
 
 CREATE TABLE users (
     id         bigserial primary key,
     username   varchar(36) not null,
     password   varchar(80) not null,
-    email      varchar(50) unique
+    email      varchar(50) unique,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
 );
 
 CREATE TABLE roles (
     id         bigserial primary key,
-    name       varchar(50) not null
+    name       varchar(50) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
 );
 
 CREATE TABLE users_roles (
     user_id    bigint not null references users (id),
     role_id    bigint not null references roles (id),
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     primary key (user_id, role_id)
 );
 
